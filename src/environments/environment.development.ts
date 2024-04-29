f1f769013e6a0fc75a1f43e6f7ee89c5d90b48be
Nguyễn Phạm Thành Nam <nath.nguyen002@gmail.com>
2024-04-29 14:42:01 +0700

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDF45YrGP88VIP4Sf8WbulTnTfs7lajgwQ",
+    authDomain: "crud-app-c92ba.firebaseapp.com",
+    projectId: "crud-app-c92ba",
+    storageBucket: "crud-app-c92ba.appspot.com",
+    messagingSenderId: "667429250303",
+    appId: "1:667429250303:web:d35695e7310000bbc398ca",
+    measurementId: "G-LK80TWJ5DW"
+  },
+};
